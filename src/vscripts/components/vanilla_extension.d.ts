68b877b8e8b34df1282c1a6f147ed42d71d0ef47
Jaden Young <me@jaden-young.com>
2022-02-06 19:32:03 -0600

    Remove duplication between vanilla_extension and extended_api

---
+++ src/vscripts/components/vanilla_extension.d.ts
@@ -0,0 +1,21 @@
+export {}
+declare global {
+    interface CDOTA_BaseNPC {
+        IsCustomHero(): boolean;
+        HasTalent(talentName: string): boolean;
+        FindTalentValue(talentName: string, key?: string): number;
+        HighestTalentTypeValue(talentType: string): number;
+        IsRoshan(): boolean;
+        // also overrides CreateIllusions
+    }
+    interface CDOTABaseAbility {
+        GetTalentSpecialValueFor(value: string): number;
+    }
+    /**
+     * Has something to do with talent initialization and
+     * `modifier_custom_mechanics` that is needed for the e.g. FindTalentValue()
+     * extension methods to work.
+     * @param hero short hero name, e.g. "naruto"
+     */
+    function CreateEmptyTalents(this: void, hero: string): void;
+}
