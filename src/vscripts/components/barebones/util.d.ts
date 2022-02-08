289897887ecc8d64d66afb5b95562f7512011067
Jaden Young <me@jaden-young.com>
2022-02-07 21:26:03 -0600

    Update barebones util types

--- src/vscripts/components/barebones/util.d.ts
+++ src/vscripts/components/barebones/util.d.ts
@@ -1,18 +1,13 @@
+/** @noSelfInFile */
 export { };
 declare global {
-    /**
-     * actually takes any number of arguments. Prints if BAREBONES_DEBUG_SPEW is true
-     */
-    function DebugPrint(this: void, arg1?: any, arg2?: any, arg3?: any, arg4?: any): void;
-    /**
-     * actually takes any number of arguments. Prints if BAREBONES_DEBUG_SPEW is true
-     */
-    function DebugPrintTable(this: void, arg1?: any, arg2?: any, arg3?: any, arg4?: any): void;
-    function PrintTable(this: void, arg1?: any, arg2?: any, arg3?: any, arg4?: any): void;
+    function DebugPrint(...args: any[]): void;
+    function DebugPrintTable(...args: any[]): void;
+    function PrintTable(...args: any[]): void;
 
     /**
      * Rolls a Psuedo Random chance. If failed, chances increases, otherwise chances are reset
      * Numbers taken from https://gaming.stackexchange.com/a/290788
      */
-    function RollPseudoRandom(this: void, base_chance: number, entity: CDOTA_BaseNPC): boolean;
+    function RollPseudoRandom(base_chance: number, entity: CDOTA_BaseNPC): boolean;
 }
