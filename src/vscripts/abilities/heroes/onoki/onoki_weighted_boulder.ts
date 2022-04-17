a997df7903a8ba844c0d56400b9c45abdf8d2425
Nocturne <65226231+NocturneOfShadows@users.noreply.github.com>
2022-04-17 16:17:51 +0100

    weighted boulder cleanup + improvements

--- src/vscripts/abilities/heroes/onoki/onoki_weighted_boulder.ts
+++ src/vscripts/abilities/heroes/onoki/onoki_weighted_boulder.ts
@@ -1,5 +1,10 @@
 import { BaseAbility, BaseModifier, registerAbility, registerModifier } from "../../../lib/dota_ts_adapter"
 
+interface extra
+{
+    fx_id?: ParticleID;
+}
+
 @registerAbility()
 export class onoki_weighted_boulder extends BaseAbility {
 
@@ -29,9 +34,9 @@ export class onoki_weighted_boulder extends BaseAbility {
         direction = direction.Normalized();
 
         this.rock_fx = ParticleManager.CreateParticle("particles/units/heroes/onoki/onoki_weighted_boulder_technique.vpcf", ParticleAttachment.ABSORIGIN, caster);
-        ParticleManager.SetParticleControl(this.rock_fx, 0, caster.GetAttachmentOrigin(caster.ScriptLookupAttachment("attach_hitloc")) + direction * 100 as Vector);
+        ParticleManager.SetParticleControl(this.rock_fx, 0, caster.GetAttachmentOrigin(caster.ScriptLookupAttachment("attach_hitloc")) + direction * 125 as Vector);
 
-        EmitSoundOn("Hero_Onoki.AtomicDismantling.Precast", caster);
+        EmitSoundOn("Hero_Onoki.WeightedBoulder.Precast", caster);
         return true;
     }
 
@@ -48,36 +53,45 @@ export class onoki_weighted_boulder extends BaseAbility {
         let caster = this.GetCaster();
         let position = this.GetCursorPosition();
         let origin = caster.GetAbsOrigin();
-        let speed = this.GetSpecialValueFor("speed")
+        let speed = this.GetSpecialValueFor("speed");
+        let min_distance = this.GetSpecialValueFor("min_range");
+        let distance = (position - caster.GetAbsOrigin() as Vector).Length2D();
+        let fx_id = this.rock_fx;
 
         let direction = position - caster.GetAbsOrigin() as Vector;
         direction.z = 0;
         direction = direction.Normalized();
 
+        position = distance >= min_distance ? position : direction * min_distance + origin as Vector;
+
         ProjectileManager.CreateLinearProjectile({
             Ability: this,
             EffectName: "",
-            vSpawnOrigin: caster.GetAttachmentOrigin(caster.ScriptLookupAttachment("attach_hitloc")) + direction * 75 as Vector,
+            vSpawnOrigin: caster.GetAttachmentOrigin(caster.ScriptLookupAttachment("attach_hitloc")) as Vector,
             fDistance: (position - origin as Vector).Length2D(),
             Source: caster,
             iUnitTargetTeam: UnitTargetTeam.NONE,
             vVelocity: direction * speed as Vector,
+            ExtraData: {
+                fx_id: fx_id
+            }
         });
 
         EmitSoundOn("Hero_Onoki.WeightedBoulder.Cast", caster);
-        ParticleManager.SetParticleControl(this.rock_fx!, 1, GetGroundPosition(position, undefined) + Vector(0, 0, 75) as Vector);
-        ParticleManager.SetParticleControl(this.rock_fx!, 2, Vector(speed, 0, 0));
+        ParticleManager.SetParticleControl(fx_id!, 1, GetGroundPosition(position, undefined) + Vector(0, 0, 75) as Vector);
+        ParticleManager.SetParticleControl(fx_id!, 2, Vector(speed, 0, 0));
     }
 
     /****************************************/
 
-    OnProjectileHit(target: CDOTA_BaseNPC | undefined, location: Vector): boolean | void {
+    OnProjectileHit_ExtraData(target: CDOTA_BaseNPC | undefined, location: Vector, extraData: extra): boolean | void {
         if (target || !this || this.IsNull()) return;
 
         let caster = this.GetCaster()
         let radius = this.GetSpecialValueFor("radius");
         let duration = this.GetSpecialValueFor("stun_duration_per_armor") * caster.GetPhysicalArmorValue(false);
         let damage = this.GetSpecialValueFor("damage_per_armor") * caster.GetPhysicalArmorValue(false);
+        let fx_id = extraData.fx_id;
 
         let damage_table : ApplyDamageOptions = {
             attacker: caster,
@@ -103,61 +117,11 @@ export class onoki_weighted_boulder extends BaseAbility {
             damage_table.victim = enemy;
             ApplyDamage(damage_table);
             enemy.AddNewModifier(caster, this, "modifier_stunned", {duration: duration * (1 - enemy.GetStatusResistance())});
+            EmitSoundOn("Hero_Onoki.WeightedBoulder.Stun", enemy);
         });
 
         EmitSoundOnLocationWithCaster(location, "Hero_Onoki.WeightedBoulder.Impact", caster);
-        ParticleManager.DestroyParticle(this.rock_fx!, false);
-        ParticleManager.ReleaseParticleIndex(this.rock_fx!);
-    }
-
-    /****************************************/
-
-    ApplySecondEffect(location: Vector) {
-        if (!this || this.IsNull()) return;
-
-        let caster = this.GetCaster()
-        let radius = this.GetSpecialValueFor("radius");
-        let stun_duration = this.GetSpecialValueFor("stun_duration");
-
-        let enemies = FindUnitsInRadius(
-            caster.GetTeamNumber(),
-            location,
-            undefined,
-            radius,
-            UnitTargetTeam.ENEMY,
-            UnitTargetType.BASIC + UnitTargetType.HERO,
-            UnitTargetFlags.NONE,
-            FindOrder.ANY,
-            false
-        );
-
-        enemies.forEach(enemy => {
-            enemy.AddNewModifier(caster, this, "modifier_stunned", {duration: stun_duration * (1 - enemy.GetStatusResistance())});
-            EmitSoundOn("Hero_Onoki.AtomicDismantling.Stun", enemy);
-        });
-    }
-}
-
-@registerModifier()
-export class modifier_onoki_weighted_boulder extends BaseModifier
-{
-    IsPurgable(): boolean       {return true}
-
-    /****************************************/
-
-    CheckState(): Partial<Record<ModifierState, boolean>> {
-        return {[ModifierState.ROOTED]: true};
-    }
-
-    /****************************************/
-
-    GetEffectName(): string {
-        return "particles/units/heroes/hero_treant/treant_bramble_root.vpcf";
-    }
-
-    /****************************************/
-    
-    GetEffectAttachType(): ParticleAttachment {
-        return ParticleAttachment.ABSORIGIN;
+        ParticleManager.DestroyParticle(fx_id!, false);
+        ParticleManager.ReleaseParticleIndex(fx_id!);
     }
 }
