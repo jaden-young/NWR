41f67e29729abf627f067f6df2fed24a83c6eea7
Nocturne <65226231+NocturneOfShadows@users.noreply.github.com>
2022-04-27 16:00:54 +0100

    afterimage clone

--- src/vscripts/abilities/heroes/shisui/shisui_afterimage_clone.ts
+++ src/vscripts/abilities/heroes/shisui/shisui_afterimage_clone.ts
@@ -1,11 +1,19 @@
 import { BaseAbility, BaseModifier, registerAbility, registerModifier } from "../../../lib/dota_ts_adapter"
 
+interface AfterimageModifier extends CDOTA_Buff
+{
+    penalty_status?: boolean;
+}
 
 @registerAbility()
 export class shisui_afterimage_clone extends BaseAbility {
 
+    clone?: CDOTA_BaseNPC;
+
+    /****************************************/
+
     Precache(context: CScriptPrecacheContext): void{
-        //PrecacheResource("particle",  "", context);
+        PrecacheResource("particle",  "particles/units/heroes/shisui/shisui_afterimage_clone_slash.vpcf", context);
         PrecacheResource("soundfile", "soundevents/heroes/shisui/game_sounds_shisui.vsndevts", context);
         //PrecacheResource("soundfile", "soundevents/heroes/shisui/game_sounds_vo_shisui.vsndevts", context);
     }
@@ -23,19 +31,22 @@ export class shisui_afterimage_clone extends BaseAbility {
         let caster = this.GetCaster();
         let position = this.GetCursorPosition();
         let radius = this.GetSpecialValueFor("radius");
+        let attack_count = this.GetSpecialValueFor("attack_count") + caster.FindTalentValue("special_bonus_shisui_5");
+
         let enemies = FindUnitsInRadius(
             caster.GetTeamNumber(),
             position,
             undefined,
-            this.GetSpecialValueFor("radius"),
+            radius,
             this.GetAbilityTargetTeam(),
             this.GetAbilityTargetType(),
-            this.GetAbilityTargetFlags(),
+            UnitTargetFlags.NO_INVIS + UnitTargetFlags.NOT_ATTACK_IMMUNE,
             FindOrder.ANY,
             false
         );
 
         if (enemies.length == 0) return;
+        this.SetActivated(false);
 
         let kv = {
             outgoing_damage: -100,
@@ -43,14 +54,186 @@ export class shisui_afterimage_clone extends BaseAbility {
             duration: 5
         }
         
-        let clone = CreateIllusions(caster, caster as CDOTA_BaseNPC_Hero, kv, 1, 0, false, false)[0];
-        clone.AddNewModifier(caster, this, "modifier_shisui_afterimage_clone", {duration: 5});
-        clone.AddNewModifier(caster, this, "modifier_kill", {duration: 5});
+        this.clone = CreateIllusions(caster, caster as CDOTA_BaseNPC_Hero, kv, 1, 0, false, false)[0];
+        this.clone.AddNewModifier(caster, this, "modifier_shisui_afterimage_clone_image", {duration: 5});
+        this.clone.AddNewModifier(caster, this, "modifier_kill", {duration: 5});
+        this.clone.SetForwardVector((position - this.clone.GetAbsOrigin() as Vector).Normalized());
+
+
+        caster.AddNewModifier(caster, this, "modifier_shisui_afterimage_clone", {duration: -1})
+
+        let heroes = FindUnitsInRadius(
+            caster.GetTeamNumber(),
+            position,
+            undefined,
+            radius,
+            this.GetAbilityTargetTeam(),
+            UnitTargetType.HERO,
+            UnitTargetFlags.NO_INVIS + UnitTargetFlags.NOT_ATTACK_IMMUNE,
+            FindOrder.ANY,
+            false
+        );
+
+        let enemy = heroes.length > 0 ? heroes[0] : enemies[0];
+        this.PerformJump(enemy, attack_count, {} as Record<EntityIndex, boolean>, position, this.GetSpecialValueFor("delay"));
+    }
+
+    /****************************************/
+
+    PerformJump(target: CDOTA_BaseNPC | undefined, attack_count: number, affected_enemies: Record<EntityIndex, boolean>, position: Vector, delay: number) {
+        let caster = this.GetCaster();
+        target = target as CDOTA_BaseNPC;
+
+        let pos = target.GetAbsOrigin() + target.GetForwardVector() * 150 as Vector;
+        pos = RotatePosition(target.GetAbsOrigin(), QAngle(0, RandomInt(0, 360), 0), pos);
+        
+        caster.SetAbsOrigin(pos);
+        caster.SetForwardVector((target.GetAbsOrigin() - caster.GetAbsOrigin() as Vector).Normalized());
+
+        caster.StartGestureWithPlaybackRate(GameActivity.DOTA_ATTACK, 1.5); //test
+        //caster.StartGesture(GameActivity.DOTA_ATTACK_SPECIAL);
+
+        let modifier = caster.FindModifierByName("modifier_shisui_afterimage_clone") as AfterimageModifier;
+
+        modifier.penalty_status = affected_enemies[target.entindex()] ? true : false;
+
+        Timers.CreateTimer(delay, () => this.AttackTarget(target, attack_count, affected_enemies, position));
+    }
+
+    /****************************************/
+
+    AttackTarget(target: CDOTA_BaseNPC | undefined, attack_count: number, affected_enemies: Record<EntityIndex, boolean>, position: Vector): void {
+        let caster = this.GetCaster();
+        target = target as CDOTA_BaseNPC;
+
+        if (!target.IsInvisible() && !target.IsAttackImmune() && !target.IsInvulnerable()) {
+            caster.PerformAttack(target, false, true, true, false, false, false, false);
+            ParticleManager.ReleaseParticleIndex(
+                ParticleManager.CreateParticle("particles/units/heroes/shisui/shisui_afterimage_clone_slash.vpcf", ParticleAttachment.ABSORIGIN, target)
+            )
+        }
+
+        attack_count--;
+        affected_enemies[target.entindex()] = true;
+
+        if (attack_count > 0) this.FindNextTarget(position, attack_count, affected_enemies);
+        else this.StopAfterimage();
+    }
+
+    /****************************************/
+
+    FindNextTarget(position: Vector, attack_count: number, affected_enemies: Record<EntityIndex, boolean>) {
+        let caster = this.GetCaster();
+        let radius = this.GetSpecialValueFor("radius");
+        let target: CDOTA_BaseNPC | undefined = undefined;
+
+        let enemies = FindUnitsInRadius(
+            caster.GetTeamNumber(),
+            position,
+            undefined,
+            radius,
+            this.GetAbilityTargetTeam(),
+            this.GetAbilityTargetType(),
+            UnitTargetFlags.NO_INVIS + UnitTargetFlags.NOT_ATTACK_IMMUNE,
+            FindOrder.ANY,
+            false
+        );
+
+        let heroes = FindUnitsInRadius(
+            caster.GetTeamNumber(),
+            position,
+            undefined,
+            radius,
+            this.GetAbilityTargetTeam(),
+            UnitTargetType.HERO,
+            UnitTargetFlags.NO_INVIS + UnitTargetFlags.NOT_ATTACK_IMMUNE,
+            FindOrder.ANY,
+            false
+        );
+
+        if (enemies.length == 0) {
+            this.StopAfterimage();
+            return;
+        }
+
+        for (let [k, enemy] of Object.entries(heroes)) {
+            if (affected_enemies[enemy.entindex()]) continue;
+            target = enemy;
+            break;
+        }
+
+        if (!target) {
+            if (heroes.length > 0) target = heroes[RandomInt(0, heroes.length -1)];
+            else {
+                for (let [k, enemy] of Object.entries(enemies)) {
+                    if (affected_enemies[enemy.entindex()]) continue;
+                    target = enemy;
+                    break;
+                }
+            }
+        }
+
+        target = target ? target : enemies[RandomInt(0, heroes.length -1)];
+
+        this.PerformJump(target, attack_count, affected_enemies, position, this.GetSpecialValueFor("delay"));
+    }
+
+    /****************************************/
+
+    StopAfterimage() {
+        let caster = this.GetCaster();
+        let origin = this.clone!.GetAbsOrigin();
+        let forward = this.clone!.GetForwardVector();
+        this.clone?.ForceKill(false);
+
+        caster.SetAbsOrigin(origin);
+        caster.SetForwardVector(forward);
+
+        caster.RemoveModifierByName("modifier_shisui_afterimage_clone");
+        this.SetActivated(true);
     }
 }
 
 @registerModifier()
 export class modifier_shisui_afterimage_clone extends BaseModifier
+{
+    damage_penalty?: number;
+    penalty_status: boolean = false;
+
+    /****************************************/
+
+    OnCreated(params: object): void {
+        this.damage_penalty = -this.GetAbility()!.GetSpecialValueFor("same_target_penalty");
+    }
+
+    /****************************************/
+
+    CheckState(): Partial<Record<ModifierState, boolean>> {
+        return {
+            [ModifierState.INVULNERABLE]: true,
+            [ModifierState.NO_UNIT_COLLISION]: true,
+            [ModifierState.UNSELECTABLE]: true,
+            [ModifierState.UNTARGETABLE]: true,
+            [ModifierState.NO_HEALTH_BAR]: true,
+            [ModifierState.DISARMED]: true,
+         };
+    }
+
+    /****************************************/
+
+    DeclareFunctions(){ return [
+        ModifierFunction.DAMAGEOUTGOING_PERCENTAGE
+    ]}
+
+    /****************************************/
+
+    GetModifierDamageOutgoing_Percentage(): number {
+        return this.penalty_status ? this.damage_penalty! : 0;
+    }
+}
+
+@registerModifier()
+export class modifier_shisui_afterimage_clone_image extends BaseModifier
 {
     IsHidden(): boolean     {return true}
 
@@ -65,6 +248,7 @@ export class modifier_shisui_afterimage_clone extends BaseModifier
             [ModifierState.NO_UNIT_COLLISION]: true,
             [ModifierState.UNSELECTABLE]: true,
             [ModifierState.UNTARGETABLE]: true,
+            [ModifierState.NO_HEALTH_BAR]: true,
          };
     }
 }
