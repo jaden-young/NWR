e897af015e6f81f83e36102d5f8abba504be874a
Nocturne <65226231+NocturneOfShadows@users.noreply.github.com>
2022-05-14 23:01:57 +0100

    shield%

--- game/scripts/vscripts/abilities/heroes/raikage/raikage_raiton_no_yoroi.lua
+++ game/scripts/vscripts/abilities/heroes/raikage/raikage_raiton_no_yoroi.lua
@@ -63,10 +63,10 @@ function modifier_raikage_shield:IsBuff() return true end
 function modifier_raikage_shield:OnCreated()
 	self.ability = self:GetAbility()
 	self.aoe = self.ability:GetSpecialValueFor("release_aoe")
-	self.damage = self:GetParent():GetMaxHealth() * self.ability:GetSpecialValueFor("max_health_shield") / 100
+	self.damage = self.ability:GetSpecialValueFor("release_damage")
 	self.duration = self.ability:GetSpecialValueFor("release_purge_duration")
 
-	self.shield = self.ability:GetSpecialValueFor("charge_damage_amount") + self:GetCaster():FindTalentValue("special_bonus_raikage_2")
+	self.shield = self:GetParent():GetMaxHealth() * self.ability:GetSpecialValueFor("max_health_shield") / 100
 
    -- add shield particles
    if not IsServer() then return end
@@ -148,7 +148,7 @@ function modifier_raikage_shield:Release()
 	ParticleManager:SetParticleControl(release_fx, 3, Vector(radius, 0, 0))
 	ParticleManager:ReleaseParticleIndex(release_fx)
 
-	self:applyAoeDamageSlow(caster, radius, ability:GetSpecialValueFor("release_damage"), ability:GetSpecialValueFor("release_purge_duration"), ability)
+	self:applyAoeDamageSlow(caster, radius, self.damage, ability:GetSpecialValueFor("release_purge_duration"), ability)
 end
 
 ----------------------------------------------------------------------------------------
